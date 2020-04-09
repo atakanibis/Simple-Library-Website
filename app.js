e7a6998d49b79901e4c3aa437576781626cbff78
Hasan Burak <hsirmaa@gmail.com>
2020-04-09 16:13:49 +0300

    admin route and views created

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
 var loginRouter = require('./routes/login');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 //Database Init
@@ -54,6 +55,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
+app.use('/admin', adminRouter);
 app.use('/logout', (req, res) => {
   delete req.session.user;
   res.redirect('/');
--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/addbook', function(req, res, next) {
+  res.render('admin/addbook', { title: 'Express' });
+});
+
+router.get('/listusers', function(req, res, next) {
+  res.render('admin/listusers', { title: 'Express' });
+});
+
+router.get('/changetime', function(req, res, next) {
+  res.render('admin/changetime', { title: 'Express' });
+});
+
 module.exports = router;
